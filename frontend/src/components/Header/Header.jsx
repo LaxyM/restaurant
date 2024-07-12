8afb0473b0a7032207aaf8e924ec7b777948f753
WhyMe <WhyMe@i.ua>
2024-07-12 21:53:53 +0200

    Counter done

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = () => {
+	return (
+		<div className='header'>
+			<div className='header-contents'>
+				<h2>Order your favorite food here</h2>
+				<p>
+					Discover a world of flavors with our food delivery! Fresh ingredients,
+					exquisite dishes, and fast delivery – enjoy restaurant-quality meals
+					right at home. Order now and taste the difference!
+				</p>
+				<button>View Menu</button>
+			</div>
+		</div>
+	)
+}
+
+export default Header
