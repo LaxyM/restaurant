bbaedcbb02cd104b2b6f96fa83683805a55835ea
WhyMe <WhyMe@i.ua>
2024-07-13 18:43:18 +0200

    Add registr form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,23 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 export const App = () => {
+	const [showLogin, setShowLogin] = React.useState(false)
+
 	return (
 		<>
+			{showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
 			<div className='app'>
-				<Navbar />
+				<Navbar setShowLogin={setShowLogin} />
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/cart' element={<Cart />} />
 					<Route path='/order' element={<PlaceOrder />} />
 				</Routes>
 			</div>
-				<Footer />
+			<Footer />
 		</>
 	)
 }
