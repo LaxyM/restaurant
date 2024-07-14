6e12995f74d2e8422b3bc99726ef8880c9df4d63
WhyMe <WhyMe@i.ua>
2024-07-14 11:11:32 +0200

    fix header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
 import './Header.css'
+import { assets } from '../../assets/assets'
 
 const Header = () => {
 	return (
 		<div className='header'>
+			<img src={assets.header_img} alt="" />
 			<div className='header-contents'>
 				<h2>Order your favorite food here</h2>
 				<p>
